1c99d9628d4fc646edc587a8172fe13b85c7453a
datdinh <madatk56>
2019-11-12 00:09:21 +0700

    add feature members of courses

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const directory = require('../config/api');
+
 const handleUsers = require('./user/index');
 const handleCourses= require('./courses/index');
+const handleMembers = require('./members/index');
 module.exports = (app)=>{
   app.use(directory.ROOT_API.HOME,handleUsers);
   app.use(directory.ROOT_API.COURSES,handleCourses);
+  app.use(directory.ROOT_API.COURSES,handleMembers);
 }
